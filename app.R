9c014d7b8029e7fe9b22232be577e9614f780a22
Synkronizing <josh6050@gmail.com>
2024-03-03 15:35:17 -0600

    move app to main folder to allow for use of runGitHub() function

---
+++ app.R
@@ -0,0 +1,168 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+library(tidyr)
+library(dplyr)
+library(magrittr) 
+library(ggplot2)
+library(wordcloud2)
+library(shiny)
+library(shinythemes)
+
+# Define UI
+ui <- fluidPage(
+  theme = shinytheme("journal"),
+  titlePanel("College Explorer"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("plot_selector", "Select Plot Type:",
+                  choices = c("Histogram", "Bar Chart", "Wordcloud")),
+      conditionalPanel(
+        condition = "input.plot_selector == 'Histogram'",
+        selectInput("hist_var", "Select Variable for Histogram:",
+                    choices = c("Apps", "Accept", "Enroll", "Top10perc", 
+                                "Top25perc", "Outstate", "Room.Board", "Personal",
+                                "S.F.Ratio", "perc.alumni", "Grad.Rate")),
+        sliderInput(inputId = "binslider", label = "Bin Width", min = 0, 
+                    max = 100, value = 40),
+        checkboxInput(inputId = "private_sel", label="Split histogram by type", value = FALSE, width = NULL)
+      ),
+      
+      #------------------------Bar Chart------------------------------
+      conditionalPanel(
+        condition = "input.plot_selector == 'Bar Chart'",
+        selectInput("bar_var", "Select Variable for Bar Chart:",
+                    choices = c("Apps", "Accept", "Enroll", "Top10perc", 
+                                "Top25perc", "Outstate", "Room.Board", "Personal",
+                                "S.F.Ratio", "perc.alumni", "Grad.Rate")),
+      ),
+      
+      #---------------- Word Cloud UI ----------------------------------
+      conditionalPanel(
+        condition = "input.plot_selector == 'Wordcloud'",
+        selectInput("cloud_var1", "Selection to Limit Word Cloud",
+                    choices = c("Grad.Rate", "Accept", "Enroll", "Top10perc", 
+                                "Top25perc", "Outstate", "Room.Board", "Personal",
+                                "S.F.Ratio", "perc.alumni", "Apps")),
+        sliderInput("cloud_var1amt", "Cloud Slider 1",
+                    min = 0, max = 50, value = c(0,50)),
+        selectInput("cloud_var2", "Selection to Limit Word Cloud:",
+                    choices = c("Apps", "Accept", "Enroll", "Top10perc", 
+                                "Top25perc", "Outstate", "Room.Board", "Personal",
+                                "S.F.Ratio", "perc.alumni", "Grad.Rate")),
+        sliderInput("cloud_var2amt", "Cloud slider 2",
+                    min = 0, max = 50, value = c(0,50)),
+        selectInput("cloud_view", "Select Variable to View in Word Cloud:",
+                    choices = c("Apps", "Accept", "Enroll", "Top10perc", 
+                                "Top25perc", "Outstate", "Room.Board", "Personal",
+                                "S.F.Ratio", "perc.alumni", "Grad.Rate") )
+        
+      )
+    ),
+    mainPanel(
+      uiOutput("output_plot")
+    )
+  )
+)
+
+
+unis <- read.csv("College_Data.csv")  %>%
+  mutate(Enroll.Rate = (Enroll / Accept)*100) %>% 
+  mutate(Accept.Rate = (Accept / Apps)*100)
+# Define server logic
+server <- function(input, output, session) {
+  
+  output$output_plot <- renderUI({
+    if (input$plot_selector == "Histogram") {
+      hist_output <- plotOutput("hist_plot")
+      return(hist_output)
+    } else if (input$plot_selector == "Bar Chart") {
+      barchart_output <- plotOutput("barchart_plot")
+      return(barchart_output)
+    } else if (input$plot_selector == "Wordcloud") {
+      wordcloud_output <- wordcloud2Output("wordcloud_plot")
+      return(wordcloud_output)
+    }
+  })
+
+  
+  #-------------------- hist server ------------------------------
+  # Render a histogram plot based on the selected variable
+  output$hist_plot <- renderPlot({
+    if (input$private_sel) {
+      ggplot(unis, aes(x = .data[[input$hist_var]])) +
+        geom_histogram(data = subset(unis, Private == "Yes"), 
+                       binwidth = input$binslider, color = "black", fill = "skyblue") +
+        geom_histogram(data = subset(unis, Private == "No"), 
+                       binwidth = input$binslider, color = "black", fill = "salmon") +
+        labs(title = paste("Histograms of", input$hist_var),
+             x = input$hist_var) +
+        facet_wrap(~ Private, labeller = labeller(Private = c(Yes = "Private", No = "Public")))
+    } else {
+      ggplot(unis, aes(x = .data[[input$hist_var]], fill = factor(Private))) +
+        geom_histogram(binwidth = input$binslider, color = "black") +
+        scale_fill_manual(values = c("skyblue", "salmon"), labels = c("Public", "Private")) +
+        labs(title = paste("Histogram of", input$hist_var),
+             x = input$hist_var,
+             fill="Type of University")
+    }
+  })
+  
+  
+  # when selected different variable change bin width settings
+  observeEvent(input$hist_var, {
+    max_val <- max(unis[[input$hist_var]])
+    min_val <- min(unis[[input$hist_var]])
+    # Update the maximum value of the slider
+    updateSliderInput(session, "binslider", min = min_val, max = max_val, value = max_val/10)
+  })
+  
+  #------------------------ bar chart server ----------------------
+  output$barchart_plot <- renderPlot({
+      barsort <- arrange(unis, desc(.data[[input$bar_var]])) %>% head(10)
+      ggplot(data=barsort,aes(x=.data[[input$bar_var]], y=X)) +
+        geom_bar(stat="identity") + geom_col(fill = "#f54242")
+  })
+  
+  # -------------------- Word cloud server -------------------------
+  output$wordcloud_plot <- renderWordcloud2({
+    # Filter the dataset based on the range sliders
+    filter_unis <- subset(unis, unis[[input$cloud_var1]] >= input$cloud_var1amt[1] &
+                            unis[[input$cloud_var1]] <= input$cloud_var1amt[2] &
+                            unis[[input$cloud_var2]] <= input$cloud_var2amt[2] &
+                            unis[[input$cloud_var2]] >= input$cloud_var2amt[1])
+    
+    # Convert word frequencies to a data frame
+    word_freq_df <- data.frame(word = filter_unis$X, 
+                               freq = filter_unis[[input$cloud_view]], stringsAsFactors = FALSE)
+    
+    # Plot the word cloud
+    wordcloud2(word_freq_df, size = 1)
+    
+  })
+  
+  observeEvent(input$cloud_var1, {
+    max_val <- max(unis[[input$cloud_var1]])
+    min_val <- min(unis[[input$cloud_var1]])
+    # Update the maximum value of the slider
+    updateSliderInput(session, "cloud_var1amt", paste("Range for",input$cloud_var1), 
+                      min = min_val, max = max_val, value = c((max_val+min_val)/2, max_val))
+  })
+  
+  observeEvent(input$cloud_var2, {
+    max_val <- max(unis[[input$cloud_var2]])
+    min_val <- min(unis[[input$cloud_var2]])
+    # Update the maximum value of the slider
+    updateSliderInput(session, "cloud_var2amt", paste("Range for",input$cloud_var2),
+                      min = min_val, max = max_val, value = c(min_val, (max_val+min_val)/2))
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
